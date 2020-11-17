eef74d649d31e6898ea172be5e6c60bfc068903d
Sakura Ayame Kinomoto <55733660+phamquocduy1110@users.noreply.github.com>
2020-11-17 15:09:30 +0700

    - Create project LearnFirebase - Create activity_register - Create et_bg.xml - Create ic_launcher.xml - Create btn_bg.xml - Create btn_cancel_bg.xml - Create activity_login.xml - Create class LoginActivity - Create class RegisterActivity - Create ic_eye - Create - Create project LearnFirebase - Create activity_register - Create et_bg.xml - Create ic_launcher.xml - Create btn_bg.xml - Create btn_cancel_bg.xml - Create ic_user.xml - Create HomeActivity - Create activity_home.xml - Code functions for LoginActivity - Create onCreate for RegisterActivity

--- app/src/main/java/com/example/learnfirebase/LoginActivity.java
+++ app/src/main/java/com/example/learnfirebase/LoginActivity.java
@@ -4,21 +4,86 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button register_login;
+    EditText userName, password;
+    Button login, register_login;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        register_login = (Button)findViewById(R.id.btnRegister_Login);
+        userName = (EditText) findViewById(R.id.txtUserName);
+        password = (EditText) findViewById(R.id.txtPassword);
+        login = (Button) findViewById(R.id.btnLogin);
+        register_login = (Button) findViewById(R.id.btnRegister_Login);
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                if (mFirebaseUser != null) {
+                    Toast.makeText(LoginActivity.this, "You are logged in", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Please Login", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = userName.getText().toString();
+                String pwd = password.getText().toString();
+                if (email.isEmpty()) {
+                    userName.setError("Please enter email id");
+                    userName.requestFocus();
+                } else if (pwd.isEmpty()) {
+                    password.setError("Please enter your password");
+                    password.requestFocus();
+                } else if (email.isEmpty() && pwd.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Fields Are Empty!", Toast.LENGTH_SHORT).show();
+                } else if (!(email.isEmpty() && pwd.isEmpty())) {
+                    mFirebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, "Login Error, Please Login Again", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Intent intToHome = new Intent(LoginActivity.this, HomeActivity.class);
+                                startActivity(intToHome);
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(LoginActivity.this, "Error Occurred!", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
         register_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,4 +92,11 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStart () {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
 }
+
--- app/src/main/java/com/example/learnfirebase/RegisterActivity.java
+++ app/src/main/java/com/example/learnfirebase/RegisterActivity.java
@@ -4,19 +4,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    Button cancel;
+    EditText fullName, dateOfBirth, phoneNumber, email, inputPassword, re_enterPassword, currentAmount, address, nationalID;
+    Button register, cancel;
+    RadioButton male, female;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        fullName = (EditText)findViewById(R.id.txtFullName);
+        dateOfBirth = (EditText)findViewById(R.id.txtDateOfBirth);
+        phoneNumber = (EditText)findViewById(R.id.txtPhoneNumber);
+        email = (EditText)findViewById(R.id.txtEmail);
+        inputPassword = (EditText)findViewById(R.id.txtInputPassword);
+        re_enterPassword = (EditText)findViewById(R.id.txtRe_enterPassword);
+        currentAmount = (EditText)findViewById(R.id.txtCurrentAmount);
+        address = (EditText)findViewById(R.id.txtAddress);
+        nationalID = (EditText)findViewById(R.id.txtNationalID);
+
+        register = (Button)findViewById(R.id.btnRegister);
         cancel = (Button)findViewById(R.id.btnCancel);
+
+        male = (RadioButton)findViewById(R.id.rdMale);
+        female = (RadioButton)findViewById(R.id.rdFemale);
+
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,5 +44,7 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
 }
