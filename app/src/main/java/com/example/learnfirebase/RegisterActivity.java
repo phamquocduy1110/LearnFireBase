060783ca534644d3b60565d9eb60f121e65c736d
Sakura Ayame Kinomoto <55733660+phamquocduy1110@users.noreply.github.com>
2020-11-17 13:22:22 +0700

    - Create project LearnFirebase - Create activity_register - Create et_bg.xml - Create ic_launcher.xml - Create btn_bg.xml - Create btn_cancel_bg.xml - Create activity_login.xml - Create class LoginActivity - Create class RegisterActivity

--- app/src/main/java/com/example/learnfirebase/RegisterActivity.java
+++ app/src/main/java/com/example/learnfirebase/RegisterActivity.java
@@ -4,25 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
-    Button register_login;
-
+    Button cancel;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
-        register_login = (Button)findViewById(R.id.btnRegister_Login);
-        register_login.setOnClickListener(new View.OnClickListener() {
+        cancel = (Button)findViewById(R.id.btnCancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
