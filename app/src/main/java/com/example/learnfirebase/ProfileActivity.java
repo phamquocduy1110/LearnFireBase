20c95d5e37f9790fbeeff57041965e78cce2021c
Sakura Ayame Kinomoto <55733660+phamquocduy1110@users.noreply.github.com>
2020-11-24 09:41:59 +0700

    - Call all button elements

--- app/src/main/java/com/example/learnfirebase/ProfileActivity.java
+++ app/src/main/java/com/example/learnfirebase/ProfileActivity.java
@@ -3,12 +3,35 @@ package com.example.learnfirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    EditText fullname, dateofbirth, phone, email, currentammount, address, nationalId, description;
+    Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        fullname = (EditText)findViewById(R.id.txtFullName);
+        dateofbirth = (EditText)findViewById(R.id.txtDateOfBirth);
+        phone = (EditText)findViewById(R.id.txtPhoneNumber);
+        email = (EditText)findViewById(R.id.txtEmail);
+        currentammount = (EditText)findViewById(R.id.txtCurrentAmount);
+        address = (EditText)findViewById(R.id.txtAddress);
+        nationalId = (EditText)findViewById(R.id.txtNationalID);
+        description = (EditText)findViewById(R.id.txtDescription);
+
+        save = (Button)findViewById(R.id.BtnSaveButton);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
